deb69e5c192fd8de3968acd7d56dd5aed37ece23
AJohnson38 <asjohnson1213@gmail.com>
2018-11-12 18:31:07 -0500

    Updates

---
+++ precache-manifest.a6a251ffac5a2a265561667004f3f53b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ae25f9cda9c88eb6f3b",
+    "url": "/animalgame/static/js/runtime~main.4ae25f9c.js"
+  },
+  {
+    "revision": "3e3eda0ec8f8c2f8d2cb",
+    "url": "/animalgame/static/js/main.3e3eda0e.chunk.js"
+  },
+  {
+    "revision": "705380186d4e6d38d8b0",
+    "url": "/animalgame/static/js/1.70538018.chunk.js"
+  },
+  {
+    "revision": "3e3eda0ec8f8c2f8d2cb",
+    "url": "/animalgame/static/css/main.63fc131e.chunk.css"
+  },
+  {
+    "revision": "287ed1bd54bd5e6d0bee81d43a09cf0a",
+    "url": "/animalgame/index.html"
+  }
+];
